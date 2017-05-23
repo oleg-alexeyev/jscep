86d7121588500507a5ff6908f2db246853b28875
Dave Grant <david@grant.org.uk>
2017-05-23 12:33:08 +0100

    Update X500UtilsTest.java

--- src/test/java/org/jscep/util/X500UtilsTest.java
+++ src/test/java/org/jscep/util/X500UtilsTest.java
@@ -9,12 +9,19 @@ import org.junit.Test;
 
 public final class X500UtilsTest {
     @Test
-    public void testIssue70() {
+    public void testIssue70() throws Exception {
+        // This is the original test string from issue 70.
         String dn = "CN=CA ONE,OU=Test CA,DC=OpenXPKI,DC=ORG";
         
+        // This is the minimum test case that occurs in the SCEP transaction
+        // Principal is typically created by end-user
         X500Principal principal = new X500Principal(dn);
-        X500Name name = new X500Name(principal.getName());
+        // jscep client encodes using BC encoding mechanism
+        byte[] encoded = new X500Name(principal.getName()).getEncoded();
+        // This simulates what _ought_ to happen in a server environment.
+        // It may be the case that the name is being decoded and the order changed.
+        X500Name name = X500Name.getInstance(encoded);
         
-        assertEquals("Distinguished name is unchanged", dn, name.toString());
+        assertEquals("Distinguished name is not equal", dn, name.toString());
     }
 }
