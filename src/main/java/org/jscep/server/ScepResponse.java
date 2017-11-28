de63deec76cc459534f572f852b8c09f15006c01
Oleg Alexeyev <oleg.alexeyev@f-secure.com>
2017-11-28 18:34:26 +0300

    Added getMultiValueHeaders() returning Map<String, List<String>> - handy in those environments which use such multi-values maps for headers, e.g. Spring Web MVC and Spring WebFlux.

--- src/main/java/org/jscep/server/ScepResponse.java
+++ src/main/java/org/jscep/server/ScepResponse.java
@@ -1,10 +1,12 @@
 package org.jscep.server;
 
 
+import static java.util.Collections.singletonList;
 import static javax.servlet.http.HttpServletResponse.SC_OK;
 
 import java.nio.charset.Charset;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public final class ScepResponse {
@@ -26,6 +28,14 @@ public final class ScepResponse {
 
     public Map<String, String> getHeaders() {
         return headers;
+    }
+
+    public Map<String, List<String>> getMultiValueHeaders() {
+        Map<String, List<String>> h = new HashMap<String, List<String>>();
+        for (Map.Entry<String, String> entry : headers.entrySet()) {
+            h.put(entry.getKey(), singletonList(entry.getValue()));
+        }
+        return h;
     }
 
     public void setBody(byte[] body) {
